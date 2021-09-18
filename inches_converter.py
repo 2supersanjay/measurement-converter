ba013c5fb975e2198fbba6987369916eb44d5a84
Sanjay <68604829+2supersanjay@users.noreply.github.com>
2021-09-18 09:02:44 -0400

    Add files via upload

---
+++ inches_converter.py
@@ -0,0 +1,16 @@
+name = ('inches converter')
+inches = 12
+centimeters = (inches*2.54)
+feet = (inches/12)
+
+print(name)
+print("inches:")
+print(inches)
+
+print('centimeters')
+print(centimeters)
+
+print('feet')
+print(feet)
+
+print('process complete!')
