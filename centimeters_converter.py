ba60d6e8ae525145ef47995b582b751dcca487f9
Sanjay <68604829+2supersanjay@users.noreply.github.com>
2021-09-24 18:59:44 -0400

    Update centimeters_converter.py

--- centimeters_converter.py
+++ centimeters_converter.py
@@ -1,11 +1,15 @@
-
 name = ('centimeters to inches converter')
-centimeters = 60
-inches = (centimeters*0.393701)
-feet = (inches/12)
+centimeters = int(input('Enter Centimeters'))
+
+if centimeters >= 0:
+    inches = (centimeters*0.393701)
+    feet = (inches/12)
+
 print(name)
+print(' ')
 print("centimeters:")
 print(centimeters)
+print(' ')
 print ("inches:")
 print (inches)
 
@@ -15,7 +19,9 @@ d=12
 if inches < (d):
     print('process complete!')
 else:
+   print(' ')
    print("feet:")
    print(feet)
+   print(' ')
    print('since the inches exceeded 12, I have automatically inputted feet for your conveninece!')
    print('process complete!')
